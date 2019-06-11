b11a140b15f41a6157b28b344e1807822948810a
Colin Chartier <colin.n.chartier@gmail.com>
2019-06-11 17:21:34 -0400

    Show pull status while building

--- build/build_logger.go
+++ build/build_logger.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"github.com/moby/buildkit/client"
 	"github.com/pkg/errors"
+	"io"
+	"math"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"sync"
 	"time"
@@ -20,32 +23,39 @@ type Logger interface {
 }
 
 type flatfileLogger struct {
+	mutex sync.Mutex
+
 	LogDirectory string
 
+	currVertexStatuses map[string]string
+	lastStatusLogSize  int64
+
 	openFiles        map[string]*os.File
-	openFilesMutex   sync.Mutex
 	logLineListeners []func(service, logLine string)
 }
 
 //NewFlatfileLogger builds a new Logger which writes text logs to (repository root)/logs/(service name).log
 func NewFlatfileLogger(logDirectory string) Logger {
 	return &flatfileLogger{
-		LogDirectory:     logDirectory,
-		openFiles:        make(map[string]*os.File),
-		logLineListeners: []func(service, logLine string){},
+		LogDirectory:       logDirectory,
+		openFiles:          make(map[string]*os.File),
+		currVertexStatuses: make(map[string]string),
+		logLineListeners:   []func(service, logLine string){},
 	}
 }
 
-func (logger *flatfileLogger) Log(service string, when time.Time, message ...interface{}) error {
+func (logger *flatfileLogger) logFile(service string) (*os.File, error) {
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+
 	var logFile *os.File
 
-	logger.openFilesMutex.Lock()
 	if existingFile, ok := logger.openFiles[service]; ok {
 		logFile = existingFile
 	} else {
 		err := os.MkdirAll(logger.LogDirectory, 0700)
 		if err != nil {
-			return errors.Errorf(
+			return nil, errors.Errorf(
 				"Could not make the logs output directory at %s: %s",
 				logger.LogDirectory,
 				err.Error())
@@ -54,19 +64,99 @@ func (logger *flatfileLogger) Log(service string, when time.Time, message ...int
 			filepath.Join(logger.LogDirectory, service+".log"),
 			os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		logFile.WriteString("") //wipe old logs
 		logger.openFiles[service] = logFile
 	}
-	logger.openFilesMutex.Unlock()
+	return logFile, nil
+}
+
+func (logger *flatfileLogger) Log(service string, when time.Time, message ...interface{}) error {
+	f, err := logger.logFile(service)
+	if err != nil {
+		return err
+	}
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+
 	messageString := strings.Trim(fmt.Sprint(message...), "\r\n")
-	_, err := logFile.WriteString(fmt.Sprintf("[%s] %s\n", when.In(time.Local), messageString))
+	numBytes, err := f.WriteString(fmt.Sprintf("[%s] %s\n", when.In(time.Local), messageString))
 	for _, listener := range logger.logLineListeners {
 		listener(service, messageString+"\n")
 	}
 	if err != nil {
 		return err
+	}
+	logger.lastStatusLogSize -= int64(numBytes)
+	if logger.lastStatusLogSize < 0 {
+		logger.lastStatusLogSize = 0
+	}
+	return nil
+}
+
+func humanReadableBytes(bytes int64) string {
+	suf := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
+	if bytes == 0 {
+		return fmt.Sprintf("0%s", suf[0])
+	}
+	place := math.Logb(math.Abs(float64(bytes))) / 10
+	return fmt.Sprintf("%.2f%s", float64(bytes)/math.Pow(1024, math.Floor(place)), suf[int64(place)])
+}
+
+func (logger *flatfileLogger) logStatus(service string, status *client.VertexStatus) error {
+	f, err := logger.logFile(service)
+	if err != nil {
+		return err
+	}
+
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
+
+	if logger.lastStatusLogSize > 0 {
+		_, err = f.Seek(-logger.lastStatusLogSize, io.SeekEnd)
+	}
+	if err != nil {
+		return err
+	}
+
+	var idText string
+	if strings.HasPrefix(status.ID, "sha256:") {
+		idText = status.ID[7:19]
+	} else {
+		idText = status.ID
+	}
+
+	var statusText string
+	if status.Total != 0 {
+		statusText = fmt.Sprintf("%s %s/%s", idText, humanReadableBytes(status.Current), humanReadableBytes(status.Total))
+	}  else {
+		statusText = fmt.Sprintf("%s %s", idText, humanReadableBytes(status.Current))
+	}
+	statusTextTimestamp := fmt.Sprintf("[%s]: %s", status.Timestamp.In(time.Local), statusText)
+
+	if status.Completed != nil {
+		delete(logger.currVertexStatuses, status.ID)
+		_, err = f.WriteString(statusTextTimestamp+"\n")
+	} else {
+		logger.currVertexStatuses[status.ID] = statusTextTimestamp
+	}
+	var statuses []string
+	for _, s := range logger.currVertexStatuses {
+		statuses = append(statuses, s+"\n")
+	}
+	sort.Strings(statuses)
+	written, err := f.WriteString(strings.Join(statuses, ""))
+	if err != nil {
+		return err
+	}
+	logger.lastStatusLogSize = int64(written)
+	for _, listener := range logger.logLineListeners {
+		if len(statuses) > 0 {
+			statusText = statuses[len(statuses)-1]
+			statusText = statusText[strings.Index(statusText, "]")+3:] //TODO HACK
+		}
+		listener(service, statusText+"\n")
 	}
 	return nil
 }
@@ -75,29 +165,29 @@ func (logger *flatfileLogger) ProcessStatus(service string, status *client.Solve
 	for _, v := range status.Vertexes { //e.g., [6/6] ADD app.py ./
 		if strings.Index(v.Name, "[internal]") != 0 { //TODO HACK these are annoying
 			if err := logger.Log(service, time.Now(), v.Name); err != nil {
-				return errors.Errorf(
-					"Could not write to %s's logs: %s",
-					service,
-					err.Error())
+				return errors.Errorf("Could not write to %s's logs: %s", service, err.Error())
 			}
+		}
+	}
+
+	for _, vs := range status.Statuses {
+		if err := logger.logStatus(service, vs); err != nil {
+			return errors.Errorf("Could not status to %s's logs: %s", service, err.Error())
 		}
 	}
 
 	for _, log := range status.Logs {
 		logMessage := []rune(string(log.Data))
 		if err := logger.Log(service, log.Timestamp, strings.Trim(string(logMessage), "\r\n")); err != nil {
-			return errors.Errorf(
-				"Could not write to %s's logs: %s",
-				service,
-				err.Error())
+			return errors.Errorf("Could not write to %s's logs: %s", service, err.Error())
 		}
 	}
 	return nil
 }
 
 func (logger *flatfileLogger) Close() {
-	logger.openFilesMutex.Lock()
-	defer logger.openFilesMutex.Unlock()
+	logger.mutex.Lock()
+	defer logger.mutex.Unlock()
 
 	for _, f := range logger.openFiles {
 		f.Close()
